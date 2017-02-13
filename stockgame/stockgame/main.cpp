5cb49df8aaba11e598f91afe90395df4282933a5
doug <doug@dougfrazer.com>
2017-02-12 17:58:49 -0800

    basic outline of how to analyze data

--- stockgame/stockgame/main.cpp
+++ stockgame/stockgame/main.cpp
@@ -10,6 +10,8 @@
 //-----------------------------------------------------------------------------
 // Static Data
 //-----------------------------------------------------------------------------
+// TODO: write a script to get thousands of these... for now, just 
+//       manually download a few
 char* s_fileNames[] =
 {
 	"..\\Data\\msft.csv",
@@ -19,6 +21,17 @@ char* s_fileNames[] =
 static std::vector<StockHistory> s_data;
 //-----------------------------------------------------------------------------
 
+void AnalyzeData()
+{
+	// For many trials...
+		// Look at a random 30 day period of a random stock
+	
+		// Use analytics to determine the predicted stock price at the next day
+
+		// Check the stock price at the next day
+	
+		// Adjust analytics based on the variance from the predicted outcome
+}
 
 //-----------------------------------------------------------------------------
 // Main Function
@@ -29,7 +42,7 @@ int main()
 	LoadFiles(&s_fileNames[0], arrsize(s_fileNames), s_data);
 
 	// Analyze data
-	// TODO
+	AnalyzeData();
 
 	return 0;
 }
